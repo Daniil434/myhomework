9c35d5b52634a55ae54215a3c7fd5c862539a56d
Daniil434 <78618849+Daniil434@users.noreply.github.com>
2021-02-05 23:02:08 +0300

    Add files via upload

---
+++ ghj.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+	let butPage = document.getElementById("button");
+        let butStyle = document.getElementById("Button");
+			butPage.addEventListener('click', function (){
+				window.location.href = "https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84";
+        }
+			);
+			butStyle.addEventListener('click', function(){
+				document.body.style.backgroundColor = "red";
+        }
+			);
+		}
+			);
